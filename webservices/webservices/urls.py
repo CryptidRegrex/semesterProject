44774de942990cd75a7ff2f9af36be50a87cced6
JT <JDTobiason@gmail.com>
2024-11-18 17:46:30 -0600

    Forgot to add it to the router, added it. 403 now

--- webservices/webservices/urls.py
+++ webservices/webservices/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from dndCharacterCustomizer.viewsets import CharacterViewSet
+from dndCharacterCustomizer.viewsets import UserViewSet
 
 router = DefaultRouter()
 router.register(r'characters', CharacterViewSet, basename='character')
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
