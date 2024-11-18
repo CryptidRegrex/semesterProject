1aa7e680e52968fd9737084f60d0cb79e2f58a0b
JT <JDTobiason@gmail.com>
2024-11-18 17:41:52 -0600

    When creating a new user always can't find user

--- webservices/dndCharacterCustomizer/serializers.py
+++ webservices/dndCharacterCustomizer/serializers.py
@@ -6,7 +6,9 @@ class CharacterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Character
         fields = '__all__'
-        
+
+# Default set to authorized, password we are ensuring this is a write only field. We ARE NOT GOING TO ALLOW READS FROM THIS FIELD
+# Security first LOL
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
@@ -14,5 +16,5 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'email', 'username', 'password', 'type']
         extra_kwargs = {
-            'type': {'default': 'AUTHORIZED'}  # Default to 'AUTHORIZED'
+            'type': {'default': 'AUTHORIZED'}  
         }
