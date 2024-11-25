64362e9e14ad89e68e0f2a3ca0c812547c10a9f1
JT <JDTobiason@gmail.com>
2024-11-25 17:12:21 -0600

    Inital work current commented out

--- webservices/webservices/urls.py
+++ webservices/webservices/urls.py
@@ -32,4 +32,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    #path('index/', include('index.urls'))
 ]
